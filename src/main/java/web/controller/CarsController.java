563fd6ee5dc366deedd223633b0e65118792a821
strorikaster <strorikaster@gmail.com>
2021-10-19 17:41:33 +0300

    Add files via upload
    CarController refactor

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,10 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
 
-import java.util.List;
+
 
 @Controller
 public class CarsController {
@@ -18,8 +17,7 @@ public class CarsController {
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue = "5") String count, ModelMap model) {
-        List<Car> findedCars = carsService.returnQtyOfCar(Integer.parseInt(count));
-        model.addAttribute("cars", findedCars);
+        model.addAttribute("cars", carsService.returnQtyOfCar(Integer.parseInt(count)));
         return "cars";
     }
 }
