388f52d1f1c0921e699e1631922428d2df78adca
strorikaster <strorikaster@gmail.com>
2021-10-19 09:44:02 +0300

    Add files via upload
    JavaMentor Preproject task 2.2.2 working ver 0.3

---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.List;
+
+@Controller
+public class CarsController {
+
+    @Autowired
+    private CarService carsService;
+
+    @GetMapping(value = "/cars")
+    public String printCars(@RequestParam(defaultValue = "5") String count, ModelMap model) {
+        List<Car> findedCars = carsService.returnQtyOfCar(Integer.parseInt(count));
+        model.addAttribute("cars", findedCars);
+        return "cars";
+    }
+}
---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,29 @@
+package web.dao;
+
+import web.model.Car;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CarDaoImpl implements CarDao {
+    private final List<Car> cars = new ArrayList<>();
+
+    {
+        cars.add(new Car("BMW", 5 , "black"));
+        cars.add(new Car("Porshe", 8, "red"));
+        cars.add(new Car("Ferarri", 10, "ornange"));
+        cars.add(new Car("Mercedes", 6, "white"));
+        cars.add(new Car("SKODA", 3, "gold"));
+    }
+
+    @Override
+    public List<Car> returnQtyOfCar(Integer count) {
+        if(count > 5 || count == 0 || count == null) {
+            return cars;
+        }
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+}
